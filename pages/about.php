d9225b55a60e423d1dca0b68556bcb57d94fa4ee
Andrian <andrian@klikcair.com>
2022-03-09 15:09:15 +0700

    Adding content on About page and FAQ page

---
+++ pages/about.php
@@ -0,0 +1,28 @@
+<section>
+  <span class="section-title">Perkenalkan, kami adalah janjisewa.</span>
+  <div class="section-content">
+    <p>janjisewa adalah platform listing yang memfasilitasi perjanjian sewa menyewa antara <strong>Pelaku Usaha</strong> UMKM, khususnya di bidang kuliner & groceries, dengan <strong>Penyedia Tempat</strong>.</p>
+    <p>Kami membantu <strong>Pelaku Usaha</strong> dan <strong>Penyedia Tempat</strong> untuk memiliki surat perjanjian sewa-menyewa yang sah dan tidak bertentangan dengan peraturan perundang-undangan.</p>
+    <p>Dengan begitu, <strong>Pelaku Usaha</strong> tidak perlu khawatir saat ingin menyewa kios, ruko, toko, atau bahkan lahan yang akan digunakan untuk mengembangkan bisnisnya.</p>
+    <p>Begitu juga dengan <strong>Penyedia Tempat</strong> tidak perlu takut lagi menyewakan kios, ruko, toko, atau lahan miliknya yang tersedia.</p>
+    <p>Sebab, kedua belah pihak bisa bertemu dengan mitra yang potensial dan masing-masing akan memperoleh manfaat serta keuntungan.</p>
+    <span class="tagline">Bersama janjisewa, semua <strong>tercatat</strong>, semua <strong>nyaman</strong>.</span>
+  </div>
+</section>
+<section>
+  <span class="section-title">Keunggulan janjisewa</span>
+  <div class="section-content">
+    <ul>
+        <li class="judul-list">Jika Anda ingin menyewakan tempat yang tersedia di janjisewa, Anda akan mendapatkan:</li>
+        <li>Bantuan dalam menyusun dan menyiapkan perjanjian sewa</li>
+        <li>Informasi detail calon penyewa</li>
+        <li>Perjanjian sewa yang menjamin kepentingan penyedia tempat</li>
+    </ul>
+    <ul>
+        <li class="judul-list">Jika Anda sedang mencari tempat untuk berbisnis di janjisewa, Anda akan mendapatkan:</li>
+        <li>Kemudahan dalam menemukan informasi tempat yang tersedia</li>
+        <li>Tahu harga pasaran sewa tempat</li>
+        <li>Perjanjian sewa-menyewa yang mudah dipahami</li>
+    </ul>
+  </div>
+</section>
